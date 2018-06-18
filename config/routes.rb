8c66455524fe230618816c19fc909ed7724106e3
Steve Han <shan61489@gmail.com>
2018-06-18 17:15:14 -0500

    added client authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get "/signup" => "users#new"
   post "/users" => "users#create"
 
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
   namespace :api do
     get 'caddies' => 'caddies#index'
     get 'caddies/new' => 'caddies#new'
